f075cf3252d1fee4026303772849782dd0d02406
Yagiz Tansu <yagiz.tansu99@gmail.com>
2025-06-19 08:57:19 +0200

    Add initial R script with factor and table examples
    - Created a new R script 'deneme.R'
    - Added a factor example with gender categories
    - Included a table example with counts of categories A and B
    - Demonstrated accessing elements of the table

---
+++ scripts/deneme.R
@@ -0,0 +1,7 @@
+x <- factor(c("erkek", "kadın", "kadın"))
+print(x)
+
+Y <- table(c("A", "B", "A"))
+
+print(Y[1])  # Accessing the first element of the table
+print(Y[2])  # Accessing the second element of the table
